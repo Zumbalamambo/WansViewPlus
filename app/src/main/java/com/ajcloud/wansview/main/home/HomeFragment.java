5ddca6b81e3bf3da64853c719ea7ceb22b77ef8d
mamengchao <mmc5634@outlook.com>
2018-05-14 15:35:31 +0800

    新增统一dialog和progressdialog

--- app/src/main/java/com/ajcloud/wansview/main/home/HomeFragment.java
+++ app/src/main/java/com/ajcloud/wansview/main/home/HomeFragment.java
@@ -4,6 +4,8 @@ import android.view.View;
 
 import com.ajcloud.wansview.R;
 import com.ajcloud.wansview.main.application.BaseFragment;
+import com.ajcloud.wansview.support.utils.dialog.CommonDialog;
+import com.ajcloud.wansview.support.utils.dialog.ProgressDialogManager;
 
 public class HomeFragment extends BaseFragment {
 
@@ -20,6 +22,11 @@ public class HomeFragment extends BaseFragment {
 
     @Override
     protected void initView(View rootView) {
-
+        rootView.findViewById(R.id.get).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProgressDialogManager.getDialogManager().showDialog("test", getActivity(), 10000);
+            }
+        });
     }
 }
