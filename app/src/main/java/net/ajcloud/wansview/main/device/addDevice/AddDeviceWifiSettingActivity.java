c0e26d606eab2fa3bb33465aec8aaa23c9fbfd03
mamengchao <mmc5634@outlook.com>
2018-05-28 11:26:10 +0800

    自动获取wifi

--- app/src/main/java/net/ajcloud/wansview/main/device/addDevice/AddDeviceWifiSettingActivity.java
+++ app/src/main/java/net/ajcloud/wansview/main/device/addDevice/AddDeviceWifiSettingActivity.java
@@ -2,7 +2,11 @@ package net.ajcloud.wansview.main.device.addDevice;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.text.TextUtils;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import net.ajcloud.wansview.main.application.BaseActivity;
@@ -13,6 +17,8 @@ public class AddDeviceWifiSettingActivity extends BaseActivity {
     private MaterialEditText wifiNameEditText;
     private MaterialEditText passwordEditText;
     private Button joinButton;
+
+    private WifiManager wifiManager;
     private String type;
 
     public static void start(Context context, String type) {
@@ -45,6 +51,20 @@ public class AddDeviceWifiSettingActivity extends BaseActivity {
         if (getIntent() != null) {
             type = getIntent().getStringExtra("type");
         }
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (wifiManager.isWifiEnabled()) {
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            if (wifiInfo != null) {
+                wifiNameEditText.setText(wifiInfo.getSSID().replace("\"", ""));
+            }
+        }
+
+        if (TextUtils.isEmpty(wifiNameEditText.getText().toString())){
+            wifiNameEditText.requestFocus();
+        }else {
+            passwordEditText.requestFocus();
+        }
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
     }
 
     @Override
